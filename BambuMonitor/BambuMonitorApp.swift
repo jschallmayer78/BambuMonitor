0671c2ed3451c2d967b97ac4092fc9577c2ef67d
Jörg Schallmayer <joerg@schallmayer.net>
2026-09-11 07:48:26 +0200

    Initial Commit

---
+++ BambuMonitor/BambuMonitorApp.swift
@@ -0,0 +1,32 @@
+//
+//  BambuMonitorApp.swift
+//  BambuMonitor
+//
+//  Created by Jörg Schallmayer on 11.09.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BambuMonitorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BambuMonitor/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BambuMonitor
+//
+//  Created by Jörg Schallmayer on 11.09.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
